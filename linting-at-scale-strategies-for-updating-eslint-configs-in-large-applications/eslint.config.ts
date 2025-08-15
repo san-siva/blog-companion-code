2cad7d807b5481cea5984a0316e1c576128e0d81
Santhosh Siva <santhosh.siva.oa@gmail.com>
2025-08-15 19:17:15 +0530

    fix: fixed eslint config to use legacy.config.ts file

--- linting-at-scale-strategies-for-updating-eslint-configs-in-large-applications/eslint.config.ts
+++ linting-at-scale-strategies-for-updating-eslint-configs-in-large-applications/eslint.config.ts
@@ -1,5 +1,6 @@
+import legacy from './eslint/legacy.config';
 import common from './eslint/common.config';
 import react from './eslint/react.config';
 import tests from './eslint/tests.config';
 
-export default [...common, ...tests, ...react];
+export default [...legacy, ...common, ...tests, ...react];
